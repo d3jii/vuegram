fea7a34802736c2d1f8d339de92ed18d09c2b91f
Aj Dev <ahero@heroku.com>
2015-12-12 19:13:51 +0100

    localie

--- config/auth.js
+++ config/auth.js
@@ -2,10 +2,10 @@
 
 // expose our config directly to our application using module.exports
 module.exports = { 
-    'instagramAuth' : {
-        'consumerKey'       : 'wXn5wRnM2YtUTeiLsX4KLVEMn',
-        'consumerSecret'    : '5WUMQ9P2lmiwRRIJO0WZQPTdIQLsTC4Qmqjyq3j648jOegct50',
-        'callbackURL'       : 'http://localhost:3112/login-twitter-confirm'
+        'instagramAuth' : {
+        'clientID'       : '65ebd101623d42b0b44d4470310a1fb5',
+        'clientSecret'    : 'a201f0d016174cb4a0317a4ba035cd75', 
+        'callbackurl':"http://127.0.0.1:9000/auth/instagram/callback"
     } 
 
 };
--- routes/route.js
+++ routes/route.js
@@ -10,7 +10,7 @@ function routeHandler(app,passport){
 
 
 
-	
+
 	app.get('/signup', function(req, res) { 
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
@@ -35,5 +35,16 @@ function routeHandler(app,passport){
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
+
+
+    app.get('/auth/instagram',
+  passport.authenticate('instagram'));
+
+app.get('/auth/instagram/callback', 
+  passport.authenticate('instagram', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 }
 module.exports = routeHandler;
