3b04d979f07dca4de28a5478781c1fd100beb837
Aj Dev <ahero@heroku.com>
2015-12-12 17:06:59 +0100

    All tags and tags users

--- v1/js/a.js
+++ v1/js/a.js
@@ -29,10 +29,32 @@ function updateCommentersFunc(comment,post){
 	post.images.text = text;
 	this.commenters[un].count++;
 	this.commenters[un].images.push(post.images);
-	console.log(this.commenters);
+	
+}
+
+function updateTaggedUsersFunc(taggeduser,post){
+	var un = taggeduser.user.username;
+	var profile_picture = taggeduser.user.profile_picture;
+	if(!this.taggedUsers[un]){
+		this.taggedUsers[un] = {count:0,images:[]};
+	}
+	this.taggedUsers[un].count++;
+	this.taggedUsers[un].images.push(post.images);
+}
+
+function updatePostTagsFunc(tag,post){
+	var un = tag; 
+	if(!this.tags[un]){
+		this.tags[un] = {count:0,images:[]};
+	}
+	this.tags[un].count++;
+	this.tags[un].images.push(post.images);
+	console.log(this.tags);
 }
 
 vuegramMethods.viewPostDetails = viewPostDetailsFunc;
 vuegramMethods.updateLikers = updateLikersFunc;
 vuegramMethods.updateCommenters = updateCommentersFunc;
+vuegramMethods.updateTaggedUsers = updateTaggedUsersFunc;
+vuegramMethods.updatePostTags = updatePostTagsFunc;
 
