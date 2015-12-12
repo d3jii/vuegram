0b062b4dd774526d871ece29678447b392263fe1
Aj Dev <ahero@heroku.com>
2015-12-12 18:44:02 +0100

    Passport local signup setup

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express         = require('express'),
     path            = require('path'),  
+    cookieParser    = require('cookie-parser')
     bodyParser      = require('body-parser'),  
     mongoose        = require('mongoose'),
     methodOverride  = require('method-override'),
     http            = require('http'),
+    passport        = require('passport'),
+    session         = require('express-session'),
+    flash            = require('connect-flash'),
     errorhandler    = require('errorhandler');
 
 var app = express();
@@ -14,22 +18,40 @@ mongoose.connect(dbConfig.url, function(err){
     else
         console.log("Connected");
 });
+require('./config/passport')(passport);
 
 app.set('port', process.env.PORT || 9000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); 
+app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'vuegramjs' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
+app.use(isLoggedIn);
 /*routes come in here*/
-var main_route = require('./routes/route.js')(app);
+var main_route = require('./routes/route.js')(app,passport);
 /**/
 
 app.use(function(req, res, next){
     res.render('404');
 });
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    //if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    //res.redirect('/login');
+}
 
 /* Setup the App */
 module.exports = app;
---
+++ config/auth.js
@@ -0,0 +1,11 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = { 
+    'instagramAuth' : {
+        'consumerKey'       : 'wXn5wRnM2YtUTeiLsX4KLVEMn',
+        'consumerSecret'    : '5WUMQ9P2lmiwRRIJO0WZQPTdIQLsTC4Qmqjyq3j648jOegct50',
+        'callbackURL'       : 'http://localhost:3112/login-twitter-confirm'
+    } 
+
+};
